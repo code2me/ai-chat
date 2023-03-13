3bc2e2ec27ca7368f1c3ee4df0b1cd0d07cec875
Navneet Bahuguna <navneetbahuguna007@gmail.com>
2023-03-13 18:57:49 +0530

    + redux-toolkit to createApi + connected AiChat to backend

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import { 
-  useMultiChatLogic, 
-  MultiChatSocket, 
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
   MultiChatWindow,
-} from 'react-chat-engine-advanced'
-import Header from "@/components/customHeader"
-import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm'
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
+import Ai from "@/components/customMessageForms/Ai";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
     "nav",
     "1234"
-  )
+  );
 
   return (
-    <div style={{flexBasis: "100%"}}>
+    <div style={{ flexBasis: "100%" }}>
       <MultiChatSocket {...chatProps} />
       <MultiChatWindow
         {...chatProps}
-        style={{ height: "100vh"}}
+        style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
           return (
-            <StandardMessageForm 
-              props={props} 
-              activeChat={chatProps.chat}
-            />
-          )
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
         }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- client/src/components/customMessageForms/Ai.jsx
+++ client/src/components/customMessageForms/Ai.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
-import MessageFormUI from './MessageFormUI';
+import { useState } from "react";
+import MessageFormUI from "./MessageFormUI";
+import { usePostAiTextMutation } from "@/state/api";
 
-const StandardMessageForm = ({ props, activeChat  }) => {
-  const [ message, setMessage ] = useState("");
-  const [ attachment, setAttachment ] = useState("");
-  
-  const handleChange = (e) => setMessage(e.target.value)
+const Ai = ({ props, activeChat }) => {
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const [trigger] = usePostAiTextMutation();
+
+  const handleChange = (e) => setMessage(e.target.value);
 
   const handleSubmit = async () => {
     const date = new Date()
@@ -13,20 +15,20 @@ const StandardMessageForm = ({ props, activeChat  }) => {
       .replace("T", " ")
       .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
 
-    const at = attachment ? [{blob: attachment, file: attachment.name}] : [];
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
     const form = {
       attachments: at,
       created: date,
       sender_username: props.username,
       text: message,
       activeChatId: activeChat.id,
-    }
+    };
 
     props.onSubmit(form);
+    trigger(form);
     setMessage("");
     setAttachment("");
-  }
-
+  };
   return (
     <MessageFormUI
       setAttachment={setAttachment}
@@ -35,6 +37,6 @@ const StandardMessageForm = ({ props, activeChat  }) => {
       handleSubmit={handleSubmit}
     />
   );
-}
+};
 
-export default StandardMessageForm
+export default Ai;
