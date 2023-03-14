72c36e5045832a796a8c2f5b7f0928963a0529cf
Navneet Bahuguna <navneetbahuguna007@gmail.com>
2023-03-14 20:30:19 +0530

    fixing build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs';
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   resolve: {
     alias: [{
       find: "@", replacement: path.resolve(__dirname, "src")
